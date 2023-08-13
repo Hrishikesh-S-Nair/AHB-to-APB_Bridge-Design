a051b73aa5b454abf0d8a206e9023b281c2763c0
Hrishikesh S Nair <work@hrishikeshsnair.com>
2023-08-14 03:32:26 +0530

    Add files via upload

---
+++ APB_Interface_.v
@@ -0,0 +1,36 @@
+// AHB to APG Bridge | Maven Silicon
+//
+//
+//
+// AHB Master
+// Date:25-06-2023
+//
+// By-Hrishikesh S Nair
+//    20BES7080 
+
+module APB_Interface(Pwrite,Pselx,Penable,Paddr,Pwdata,Pwriteout,Pselxout,Penableout,Paddrout,Pwdataout,Prdata);
+
+input Pwrite,Penable;
+input [2:0] Pselx;
+input [31:0] Pwdata,Paddr;
+
+output Pwriteout,Penableout;
+output [2:0] Pselxout;
+output [31:0] Pwdataout,Paddrout;
+output reg [31:0] Prdata;
+
+assign Penableout=Penable;
+assign Pselxout=Pselx;
+assign Pwriteout=Pwrite;
+assign Paddrout=Paddr;
+assign Pwdataout=Pwdata;
+
+always @(*)
+ begin
+  if (~Pwrite && Penable)
+   Prdata=($random)%256;
+  else
+   Prdata=0;
+ end
+
+endmodule
